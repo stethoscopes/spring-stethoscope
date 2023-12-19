3068bb5f547dc5fd2d92321568e2da37949e7b26
jwt1029 <jwt1029@midasin.com>
2023-12-19 09:09:23 +0900

    init project

---
+++ src/main/java/org/dev/stethoscope/SpringStethoscopeApplication.java
@@ -0,0 +1,13 @@
+package org.dev.stethoscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStethoscopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStethoscopeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dev/stethoscope/SpringStethoscopeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.stethoscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStethoscopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
